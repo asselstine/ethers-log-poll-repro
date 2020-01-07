2c2ab46877226787a57b038ba77d0d4e87af2fbc
Brendan Asselstine <mail.asselstine@gmail.com>
2020-01-07 10:28:29 -0800

    Added few more filter calls

--- index.js
+++ index.js
@@ -6,29 +6,26 @@ function loaded() {
     toBlock: "latest",
     topics: [null]
   }
+
+  const filter2 = {
+    address: "0xb7896fce748396EcFC240F5a0d3Cc92ca42D7d84",
+    fromBlock: 8485000,
+    toBlock: "latest",
+    topics: [null]
+  }
   
   window.ethereum.enable().then(function () {
     let provider = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/1aa9e694ded444ae8056a6f040a2b648', 'homestead')
   
-    let requestCount = 0
-    let lastRequestTime = new Date()
-
     document.getElementById('request-count').innerHTML = 'Ready...'
     
     provider.on('block', (blockNumber) => {
       document.getElementById('request-count').innerHTML = `Block number ${blockNumber}`
     })
 
-    provider.on(filter1, (log) => {
-      requestCount++
-      const now = new Date()
-      const elapsed = now.getTime() - lastRequestTime.getTime()
-      lastRequestTime = new Date()
-    
-      const msg = `Request count: ${requestCount}, elapsed time: ${elapsed / 1000}`
-    
-      console.log(msg)
-      document.getElementById('request-count').innerHTML = msg
-    })
+    provider.on(filter1, (log) => {})
+    provider.on(filter1, (log) => {})
+    provider.on(filter2, (log) => {})
+    provider.on(filter2, (log) => {})
   })
 }
