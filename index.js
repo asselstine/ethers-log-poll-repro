cd94768892e24ae742ccc4fcf62a3c5c87601840
Brendan Asselstine <mail.asselstine@gmail.com>
2020-01-07 10:13:32 -0800

    Changed net name to mainnet

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function loaded() {
   }
   
   window.ethereum.enable().then(function () {
-    let provider = new ethers.providers.InfuraProvider('homestead', '1aa9e694ded444ae8056a6f040a2b648')
+    let provider = new ethers.providers.InfuraProvider('mainnet', '1aa9e694ded444ae8056a6f040a2b648')
+    // provider = new ethers.providers.InfuraProvider(net.name, process.env.NEXT_JS_INFURA_ID)
   
     let requestCount = 0
     let lastRequestTime = new Date()
